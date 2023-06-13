a1683e6fea7410a58f95db1d9d7ef15dc5dc29b5
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-06-14 00:15:41 +0100

    Started configuring all routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,24 @@
-const express = require("express");
-const app = express();
 require('dotenv').config()
+const express = require("express");
+
+const app = express();
+
+// Get all Restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "Success",
+        data: {
+            restaurant: ["McDonalds", "Wendys"],
+        },
+    });
+});
+
+// Get a Restaurants
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params)
+})
+
+// Create a restaurant
 
 
 const port = process.env.PORT || 5001
