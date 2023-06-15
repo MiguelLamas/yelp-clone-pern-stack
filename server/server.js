1f4acc04a566ce6b6511063a600609d3eac00454
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-06-16 00:09:09 +0100

    Created Get a specific restaurant route

--- server/server.js
+++ server/server.js
@@ -4,85 +4,78 @@ const db = require("./db/index");
 // import morgan from "morgan";
 const app = express();
 
-// MIDDLEWEAR - HAS TO BE TOLD TO PASS ON REQUEST TO 'NEXT' PIECE OF MIDDLEWEAR/or ROUTE HANDLER LIKE BELOW
-// WE DO THAT PASSING IN A THIRD ARGUEMENT CALLED A 'NEXT' FUNCTION WHICH TELLS THE MIDDLEWEAR TO PASS ON THE REQUEST
-// TO THE NEXT MIDDLEWEAR/or ROUTE HANDLER.
-
-// app.use(morgan("dev"));
-
-// app.use((req, res, next) => {
-//     console.log('Middlewear is running');
-//     next();
-//     })
-
-// MIDDLEWARE
+// middlewear
 app.use(express.json());
 
+// route handlers
 
-// ROUTE HANDLERS
-
-// Get all Restaurants
+// get all restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
-
+  try {
     const results = await db.query("SELECT * FROM restaurants");
-    console.log("Route Handler is Running")
+    console.log(results);
     res.status(200).json({
-        status: "Success",
-        data: {
-            restaurant: ["McDonalds", "Wendys"],
-        },
+      status: "Success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
     });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-// Get a Restaurants
-app.get("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req.params)
-
+// get a specific restaurant
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "SELECT * FROM restaurants WHERE id = $1", [req.params.id]);
+      
     res.status(200).json({
-        status: "Success", 
-        data: {
-        restaurant: "McDonalds"
-        },
-    })
-})
+      status: "Success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // Create a Restaurant
 app.post("/api/v1/restaurants", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    res.status(201).json({
-        status: "Success", 
-        data: {
-        restaurant: "Wendys",
-        },
-    })
-
-})
-
+  res.status(201).json({
+    status: "Success",
+    data: {
+      restaurant: "Wendys",
+    },
+  });
+});
 
 // Update a Restaurant
 app.put("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
+  console.log(req.params.id);
+  console.log(req.body);
 
-    res.status(200).json({
-        status: "Success", 
-        data: {
-        restaurant: "Wendys",
-        },
-    })
-})
+  res.status(200).json({
+    status: "Success",
+    data: {
+      restaurant: "Wendys",
+    },
+  });
+});
 
 // Delete Restaurant
 app.delete("/api/v1/restaurants/:id", async (req, res) => {
-    
-    res.status(204).json({
-        status: "Success", 
-    })
-})
+  res.status(204).json({
+    status: "Success",
+  });
+});
 
-
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
-    console.log(`Server is up and listening on port ${port}`)
+  console.log(`Server is up and listening on port ${port}`);
 });
