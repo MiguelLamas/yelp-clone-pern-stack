f33f4c9bbe45acdd78b5404b963dd5d38bcd9811
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-06-19 22:35:10 +0100

    Working on Add Restaurant Component

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import AddRestaurant from '../components/AddRestaurant';
 
 const Home = () => {
   return <div>
     <Header />
+    <AddRestaurant />
   </div>
 }
 
