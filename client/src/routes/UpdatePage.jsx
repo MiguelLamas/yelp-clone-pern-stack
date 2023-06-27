1f19099f394592bd535528768f1175168691d233
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-06-27 23:03:49 +0100

    Completed my updateRestaurant Component

--- client/src/routes/UpdatePage.jsx
+++ client/src/routes/UpdatePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import UpdateRestaurant from "../components/UpdateRestaurant.jsx";
+import UpdateRestaurant from '../components/UpdateRestaurant.jsx';
 
 
 const UpdatePage = () => {
